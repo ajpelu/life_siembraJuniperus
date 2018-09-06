6438bee8c14092a8cb9fb392698da1f4b0e81a02
ajpelu <ajpelu@gmail.com>
2018-09-06 12:18:52 +0200

    update function

--- script/R/validaHobo.R
+++ script/R/validaHobo.R
@@ -5,32 +5,32 @@
 
 # arguments
 # x: a tibble with data from preparaHobo.R 
-# vnameTemp: name of the variable Temperature
 # vnameDate: name of the variable Date 
 
-# a) Create flags for bad Battery events. -1 = bad events, 1 = good events
+# a) Create flags for bad Battery events. Bad = bad events, Good = good events
 # b) Validate values using sensor specifications
 #       Validate temperature range (-20 to 70 C degree) in air. 
 #       See http://www.onsetcomp.com/products/data-loggers/utbi-001
-# c) Remove first and last day 
 
 
 validaHobo <- function(df, vnameTemp) { 
   
   require(dplyr)
+  require(zoo)
+
+  # Create flag column 
+  # See https://markhneedham.com/blog/2015/06/28/r-dplyr-update-rows-with-earlierprevious-rows-values/ 
+  aux <- df %>% mutate(
+    auxFlag = ifelse(is.na(BadBattery) & is.na(GoodBattery), NA, 
+                     ifelse(!is.na(BadBattery), 'Bad', 'Good')),
+    flag0 = zoo::na.locf0(auxFlag)) %>% 
+    mutate(flag = ifelse(is.na(flag0), 'Good',flag0)) %>% 
+    dplyr::select(-c(auxFlag, flag0))
   
-  # aux <- df %>% mutate(
-  #   bB = as.numeric(if_else(is.na(BadBattery), 0, -1)),
-  #   gB = as.numeric(if_else(is.na(GoodBattery), 0, 1)),
-  #   aux_flag = bB + gB,
-  #   # https://stackoverflow.com/questions/20416046/filling-data-frame-with-previous-row-value 
-  #   flag = ave(aux_flag, cumsum(aux_flag), FUN = function(x) x[x !=0])) %>% 
-  #   dplyr::select(-c(bB,gB,aux_flag))
-  # 
-  
+
   # Validate temperature range (-20 to 70 C degree) in air. 
   # See http://www.onsetcomp.com/products/data-loggers/utbi-001
-  validdf <- df %>% 
+  validdf <- aux %>% 
     mutate(valid = if_else(
       .[[vnameTemp]] < -20, 0, if_else(.[[vnameTemp]] >= 70, 0, 1)
     )) 
